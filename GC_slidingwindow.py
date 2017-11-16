27ba18d53af682ec709d0cfa479ce31acdb78775
Ward Deboutte <WD@wards-air.home>
2017-11-17 00:40:01 +0100

    Format node

--- GC_slidingwindow.py
+++ GC_slidingwindow.py
@@ -4,10 +4,10 @@ from Bio import SeqIO
 from Bio.Seq import Seq
 from Bio import SeqUtils
 import os
-print('Give the name of the fasta file, including extension')
-fasta = str(input())
-print('How would you like to call the output?')
-nameout = str(input())
+#print('Give the name of the fasta file, including extension')
+fasta = str("phages_nuc.fasta")
+#print('How would you like to call the output?')
+#nameout = str(input())
 count = 0
 changecount = 0
 gcstat = []
@@ -17,8 +17,8 @@ for seqrecord in SeqIO.parse(fasta, "fasta"):
     nowindow = length // 500
     for i in range(1,(int(nowindow)+1)):
         test = SeqUtils.GC(seqrecord.seq[(i*500)-500:(i*500)])
-        oneid = str(seqrecord.id) +' ' + str((i*500)-500) + ' ' + str((i*500)) + ' ' + str(test)
+        oneid = str(seqrecord.id.split(sep="_")[0] + seqrecord.id.split(sep="_")[1] + '_' + seqrecord.id.split(sep="_")[8] ) +' ' + str((i*500)-500) + ' ' + str((i*500)) + ' ' + str(test)
         print(oneid)
     testje = SeqUtils.GC(seqrecord.seq[((int(nowindow)*500)):])
-    twoid = str(seqrecord.id) + ' ' + str((nowindow*500)) + ' ' + str(length) + ' ' + str(round(testje,1))
+    twoid = str(seqrecord.id.split(sep="_")[0] + seqrecord.id.split(sep="_")[1] + '_' + seqrecord.id.split(sep="_")[8] ) + ' ' + str((nowindow*500)) + ' ' + str(length) + ' ' + str(round(testje,1))
     print(twoid)
