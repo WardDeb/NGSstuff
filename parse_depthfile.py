7e80fb8cb6c519f17caf1f453d81dcb8cf7eec1b
Ward Deboutte <WD@wards-air.home>
2017-11-17 02:43:14 +0100

    changed average coverage to log(avgcov)

--- parse_depthfile.py
+++ parse_depthfile.py
@@ -1,4 +1,5 @@
 #python3
+from math import log10
 num_lines = sum(1 for line in open("BP1.depth"))
 with open("BP1.depth") as f:
     tempnumbers = []
@@ -25,7 +26,7 @@ with open("BP1.depth") as f:
                 avg = round(sum(tempnumbers)/len(tempnumbers),2)
                 start = (int(keeper)*500)+1
                 stop = (int(keeper)*500)+500
-                print(str(node) + ' ' + str(start) + ' ' + str(stop)+ ' ' +str(avg))
+                print(str(node) + ' ' + str(start) + ' ' + str(stop)+ ' ' +str(log10(avg)))
                 keeper += 1
                 count = 0
         if fullnode != line.split("\t")[0]:
@@ -36,7 +37,7 @@ with open("BP1.depth") as f:
             if start == stop:
                 print("Shit man one contig is x+1 big :'(, can't deal")
                 break
-            print(str(node) + ' ' + str(start) + ' ' + str(stopreal)+ ' ' +str(avg))
+            print(str(node) + ' ' + str(start) + ' ' + str(stopreal)+ ' ' +str(log10(avg)))
             keeper = 0
             count = 0
         if fullcounty == num_lines:
@@ -50,4 +51,4 @@ with open("BP1.depth") as f:
             if start == stop:
                 print("Shit man one contig is x+1 big :'(, can't deal")
                 break
-            print(str(node) + ' ' + str(start) + ' ' + str(stopreal)+ ' ' +str(avg)) 
+            print(str(node) + ' ' + str(start) + ' ' + str(stopreal)+ ' ' +str(log10(avg))) 
