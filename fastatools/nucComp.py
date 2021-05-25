a3e31379343129f7e3d9379e0a29114cc098a14d
Warddeb <ward@deboutte.be>
2021-05-25 19:02:11 +0200

    nucComp centralize around base

--- fastatools/nucComp.py
+++ fastatools/nucComp.py
@@ -2,20 +2,35 @@ from Bio import SeqIO
 import argparse
 import os
 import csv
+import sys
 
 def slider(seq, win, step=1):
     seqlen = len(seq)
-    for i in range(0,seqlen,step):
-        j = seqlen if i+win>seqlen else i+win
-        yield seq[i:j]
-        if j==seqlen: break
+    if win % 2 != 0:
+        print("Window size has to be an even number")
+        sys.exit()
+    for i in range(0,seqlen,step): #i is 0-indexed, so add one.
+        pos = i + 1
+        halfWin = win/2
+        if pos < halfWin:
+            start = 1
+            stop = pos + halfWin
+        elif pos + halfWin > seqlen:
+            start = pos - halfWin
+            stop = seqlen
+        else:
+            start = pos - halfWin
+            stop = pos + halfWin
+        yield pos, seq[int(start):int(stop)]
+        if pos + halfWin > seqlen:
+            break
 
 def main():
     parser = argparse.ArgumentParser(description='Calculate nucleotide composition in fasta file.')
     parser.add_argument('-i', type=str, required=True,
                         help='point to fastafile')
     parser.add_argument('--window', type=int, default=100, required=False,
-                        help='Size of the sliding window.')
+                        help='Size of the sliding window. [default 100]. Needs to be an even number.')
     args = parser.parse_args()
 
     if not os.path.exists(args.i):
@@ -27,13 +42,14 @@ def main():
         tempList = []
         seq = seqrecord.seq
         tempList.append(['A','C','T','G','GC'])
-        for subseq in slider(seq, args.window,1):
+        for baseStep, subseq in slider(seq, args.window,1):
             countA = subseq.count('A')
             countC = subseq.count('C')
             countT = subseq.count('T')
             countG = subseq.count('G')
             countGC = (countC + countG)
             tempList.append([
+                baseStep,
                 countA/args.window,
                 countC/args.window,
                 countT/args.window,
